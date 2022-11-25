c367617d995db05d1fd15ad6a581810c113622a0
alejandroluzuriaga <luzuriagalejandro@gmail.com>
2022-11-25 18:34:07 +0100

    Add 01-variables.js

---
+++ 01-variables.js
@@ -0,0 +1,20 @@
+ var greeting = 'Hola esto es una prueba';
+ var pmba = ' y esto es otra :)';
+ var PMBAgreetings = greeting + pmba;
+ var ageAsString = '30';
+ var ageAsNumber = 30;
+ var arrayOfValues = [30, '30', false, true, 4];
+ var objectOfValues = {
+    age: 30,
+    ageAsNumber: ageAsNumber
+ }
+                         // Como siempre, trataremos variables en minúscula, y constantes en mayúsculas.
+console.log(ageAsNumber + ageAsString);
+console.log(PMBAgreetings);
+console.log(arrayOfValues)
+
+// string
+// number
+// boolean
+// Array
+// Object
